4e6f1268a6ba9c183a0b52a3049abba4a50c43dc
snowwhite1210 <icegold31@gmail.com>
2021-05-19 11:42:55 +0300

    second commit

---
+++ assignment.js
@@ -0,0 +1,25 @@
+var a;
+var b;
+var c;
+
+//integers
+a = 50;
+console.log(a);
+console.log(typeof(a));
+
+//string
+b = 'Dora';
+console.log(b);
+console.log(typeof(b));
+
+//float
+c = 45.555;
+console.log(c);
+console.log((c). toFixed(3));
+
+//boolean
+a = 70;
+b = 40;
+c = a < b;
+console.log(c);
+console.log(typeof(c));
---
+++ index.js
@@ -0,0 +1,28 @@
+var a; // declaration
+var c; 
+a = 5; // assignment
+
+console.log(a); //printing to the console
+
+var b = "This is javascript"; // declaration and assignment
+
+console.log(b);
+c = 100;
+console.log(c);
+
+function add(){
+
+    var e = 20;
+    var f = 30;
+    console.log(a + b);
+
+}
+
+function sub(){
+    var g = 12;
+    var h = 4;
+    console.log(g - h);
+
+}
+add();
+sub();
